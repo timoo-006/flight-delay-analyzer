89322a496c3cadb7fb24f493fae6d69500833c56
Timo Oswald <toswald@icloud.com>
2022-12-17 14:31:49 +0100

    profesionelli code qualität

--- Flight-delay-analyzer/FlightAware.cs
+++ Flight-delay-analyzer/FlightAware.cs
@@ -9,70 +9,84 @@ namespace Flight_delay_analyzer;
 
 public class FlightAware
 {
-    private IWebDriver driver;
-    private List<String> delayedFlights = new List<String>();
+    private readonly IWebDriver _driver;
+    private readonly List<string> _delayedFlights = new();
     
     // Property to get the list of delayed flights
-    public List<String> DelayedFlights
-    {
-        get { return delayedFlights; }
-    }
+    public List<string> DelayedFlights => _delayedFlights;
 
     public FlightAware(string originAirport, string destinationAirport)
     {
-        new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
-        driver = new ChromeDriver();
+        try
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            _driver = new ChromeDriver();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("There was an error with Initializing Selenium. Make Sure you have the correct Version of Chrome installed. (" + e.Message + ")");
+            Environment.Exit(0);
+        }
 
-        // Navigate to the FlightAware website
-        driver.Navigate().GoToUrl("https://de.flightaware.com/live/findflight?origin=" + originAirport + "&destination=" + destinationAirport);
-        
+        try
+        {
+            // Navigate to the FlightAware website
+            _driver.Navigate().GoToUrl("https://de.flightaware.com/live/findflight?origin=" + originAirport + "&destination=" + destinationAirport);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("There was an error with navigating to the FlightAware website. (" + e.Message + ")");
+            Environment.Exit(0);
+        }
+
         // Set the window size to square
-        driver.Manage().Window.Size = new System.Drawing.Size(1000, 1000);
+        _driver.Manage().Window.Size = new System.Drawing.Size(1000, 1000);
 
         FilterFlights();
     }
 
     public void GetFlights()
     {
-        List<IWebElement> flightRows = GetFlightRows();
-        List<String> flights = new List<String>();
+        var flightRows = GetFlightRows();
+        var flights = new List<string>();
 
-        foreach (IWebElement flightRow in flightRows)
+        foreach (var flightRow in flightRows)
         {
             List<IWebElement> flightData = flightRow.FindElements(By.TagName("td")).ToList();
             
             // Find the elements that has a anchor tag and get the href attribute
-            String flightNumber = flightData[1].FindElement(By.TagName("a")).GetAttribute("href");
+            var flightNumber = flightData[1].FindElement(By.TagName("a")).GetAttribute("href");
 
             flights.Add(flightNumber);
         }
         
+        // TODO: Remove the Console.WriteLine
         Console.WriteLine("--- Flights ---");
         Console.Write("Total flights: " + flights.Count);
         Console.WriteLine();
 
         // Get the delay of each flight
-        foreach (String flight in flights)
+        foreach (var flight in flights)
         {
             GetFlightDelay(flight);
         }
         
-        driver.Close();
+        _driver.Close();
     }
     
-    private void GetFlightDelay(String flight)
+    private void GetFlightDelay(string flight)
     {
         // Open the flight page
-        driver.Navigate().GoToUrl(flight);
+        _driver.Navigate().GoToUrl(flight);
         
         // Wait for the page to load
-        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+        var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(2));
         
         // Get the delay element
-        String delay = driver.FindElement(By.XPath("//div[@class='flightPageDestinationDelayStatus']/span")).Text;
+        var delay = _driver.FindElement(By.XPath("//div[@class='flightPageDestinationDelayStatus']/span")).Text;
         
         // Find the flight number
-        String flightNumber = driver.FindElements(By.TagName("h1")).First().Text;
+        var flightNumber = _driver.FindElements(By.TagName("h1")).First().Text;
         
         // Cut off the brackets and the Verspätung text
         try
@@ -88,7 +102,8 @@ public class FlightAware
         // If the flight is delayed add it to the list
         if (delay != "(pünktlich)" && !delay.Contains("verfrüht"))
         {
-            delayedFlights.Add(delay + " / " + flightNumber);
+            // TODO: Remove the Console.WriteLine
+            _delayedFlights.Add(delay + " / " + flightNumber);
             Console.WriteLine(delay + " / " + flightNumber);
         }
     }
@@ -96,23 +111,23 @@ public class FlightAware
     private void FilterFlights()
     {
         // Close the Cookie popup.
-        driver.FindElement(By.Id("cookieDisclaimerButtonText")).Click();
+        _driver.FindElement(By.Id("cookieDisclaimerButtonText")).Click();
         
         // Open the filter menu
-        driver.FindElement(By.XPath("//div[@id='ffinder-refine']/form/div[2]/a")).Click();
+        _driver.FindElement(By.XPath("//div[@id='ffinder-refine']/form/div[2]/a")).Click();
         
         // Apply the filter for only flights that have already landed.
-        driver.FindElement(By.XPath("//li[div/label[contains(.,'angekommen')]]/div[2]/a")).Click();
+        _driver.FindElement(By.XPath("//li[div/label[contains(.,'angekommen')]]/div[2]/a")).Click();
         
         // Apply the filter for only flights that have landed yesterday.
-        driver.FindElements(By.XPath("//li[div/label[contains(.,'Gestern')]]/div[2]/a"))[1].Click();
+        _driver.FindElements(By.XPath("//li[div/label[contains(.,'Gestern')]]/div[2]/a"))[1].Click();
     }
 
     private List<IWebElement> GetFlightRows()
     {
-        IWebElement table = driver.FindElement(By.Id("Results"));
-        IWebElement tbody = table.FindElement(By.TagName("tbody"));
-        IList<IWebElement> rows = tbody.FindElements(By.TagName("tr"));
+        var table = _driver.FindElement(By.Id("Results"));
+        var tbody = table.FindElement(By.TagName("tbody"));
+        var rows = tbody.FindElements(By.TagName("tr"));
         
         // Filter out all empty rows
         return rows.Where(row => row.Text != "").ToList();
