de2f010494c3f881168a50f7a3d8e66a3109739f
Shamoun Hennawi <shamuonhennawi05@gmail.com>
2023-01-09 20:38:28 +0100

    Latest and earlist flight stored in variable

--- Flight_delay_analyzer/JSONReadAndWrite.cs
+++ Flight_delay_analyzer/JSONReadAndWrite.cs
@@ -12,12 +12,22 @@ using System.Threading.Tasks;
 
 namespace Flight_delay_analyzer
 {
+    
     class JSONReadAndWrite
     {
+        internal class FlightsAnalyzeProperties
+        {
+            public string FlightNumber { get; set; }
+            public int FlightDelay { get; set; }
+        }
+
         /// <summary>
         /// Reads the flight code and stores it into a JSON file
         /// </summary>
-        /// <returns>deserializedJson</returns>
+        /// <returns>List<JSONInputs></returns>
+        /// <param name="originAirport"></param>
+        /// <param name="destinationAirport"></param>
+        /// <param name="dateOfFlight"></param>
         public List<JSONInputs> ReadFlightsOutFromJSON(string originAirport, string destinationAirport, DateTime dateOfFlight)
         {
             try
@@ -47,7 +57,7 @@ namespace Flight_delay_analyzer
                 var delayedFlightsIntoJson = JsonConvert.SerializeObject(delayedFlights);
                 File.AppendAllText("delayedFlights.json", delayedFlightsIntoJson);
 
-                if (delayedFlights.Count <= 0)
+                if (delayedFlights.Count == 0)
                 {
                     throw new Exception("No flights detected");
                 }
@@ -60,19 +70,41 @@ namespace Flight_delay_analyzer
 
         public void AnalyzeResults(List<Flight> flightList)
         {
+         
+            List<FlightsAnalyzeProperties> flightsToAnalyze = new List<FlightsAnalyzeProperties>();
+
             foreach (Flight flight in flightList)
             {
+                FlightsAnalyzeProperties flightAnalyzeObject = new FlightsAnalyzeProperties();
                 if (flight.delay.Contains("verspätet"))
-{
+                {
                     int minutesOfDelay = Convert.ToInt32(new string(flight.delay.Where(char.IsDigit).ToArray()).ToString());
-                    string delayedFlightNumber = flight.flightNumber;
+                    string flightNumber = flight.flightNumber;
+                    flightAnalyzeObject.FlightDelay = minutesOfDelay;
+                    flightAnalyzeObject.FlightNumber = flightNumber;
+                    flightsToAnalyze.Add(flightAnalyzeObject);
                 }
                 if (flight.delay.Contains("verfrüht"))
                 {
-                    int minutesOfDelay = Convert.ToInt32(new string(flight.delay.Where(char.IsDigit).ToArray()).ToString());
-                    string delayedFlightNumber = flight.flightNumber;
+                    //Turn delay to negative number to detect if flight is too late or too early
+                    int minutesOfDelay = Convert.ToInt32(new string(flight.delay.Where(char.IsDigit).ToArray()).ToString()) * -1;
+                    string flightNumber = flight.flightNumber;
+                    flightAnalyzeObject.FlightDelay = minutesOfDelay;
+                    flightAnalyzeObject.FlightNumber = flightNumber;
+                    flightsToAnalyze.Add(flightAnalyzeObject);
+                }
+                else
+                {
+                    int minutesOfDelay = 0;
+                    string flightNumber = flight.flightNumber;
+                    flightAnalyzeObject.FlightDelay = minutesOfDelay;
+                    flightAnalyzeObject.FlightNumber = flightNumber;
+                    flightsToAnalyze.Add(flightAnalyzeObject);
                 }
             }
+            int biggestDelay = flightsToAnalyze.Max(flight => flight.FlightDelay);
+             
+            int earliestAnticipatedFlight = flightsToAnalyze.Min(flightList => flightList.FlightDelay);
         }
     }
 }
