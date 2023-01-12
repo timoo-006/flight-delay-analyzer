ea769bc3b1666b1d41b073d63194bf86a17246e4
Shamoun Hennawi <shamuonhennawi05@gmail.com>
2023-01-12 23:50:04 +0100

    Acceptence tests

---
+++ JSOnReadAndWrite_AcceptenceTest/Features/JSONReadAndWrite.feature
@@ -0,0 +1,11 @@
+﻿Feature: JSONReadAndWrite
+
+Scenario: Return List of OriginAirport and DestinationAirport
+	Given an OriginAirport and DestinationAirport,
+	When the method ReadFlightsOutFromJSON(originAirport, destinationAirport, dateOfFlight) is called,
+	Then the method should return a list of JSONInputs
+	
+Scenario: Throw exception because of invalid Inputs
+	Given a false OriginAirport and DestinationAirport,
+	When the method ReadFlightsOutFromJSONoriginAirport, destinationAirport, dateOfFlight is called,
+	Then the method should throw an exception
---
+++ JSOnReadAndWrite_AcceptenceTest/Steps/JSOnReadAndWriteStepDefinitions.cs
@@ -0,0 +1,56 @@
+﻿using Flight_delay_analyzer.Storage;
+using FluentAssertions;
+
+namespace JSOnReadAndWrite_AcceptenceTest.Steps;
+
+[Binding]
+public sealed class JSOnReadAndWriteStepDefinitions
+{
+
+    private JSONReadAndWrite jsonReadAndWriteObject;
+    private string originAirport = string.Empty;
+    private string destinationAirport = string.Empty;
+    private DateTime dateOfFlight = DateTime.Now.AddDays(-1);
+    private List<JSONInputs> jsonInputsList;
+    private Action invalidMethodArguments;
+
+    [Given(@"an OriginAirport and DestinationAirport")]
+    public void GivenAValidJsonFile()
+    {
+        originAirport = "AMS";
+        destinationAirport = "FRA";
+    }
+    
+    [When(@"the method ReadFlightsOutFromJSON\(originAirport, destinationAirport, dateOfFlight\) is called,")]
+    public void WhenTheMethodReadFlightsOutFromJsonOriginAirportDestinationAirportDateOfFlightIsCalled()
+    {
+        jsonReadAndWriteObject.ReadFlightsOutFromJSON(originAirport, destinationAirport, dateOfFlight);
+    }
+
+    [Then(@"the method should return a list of JSONInputs")]
+    public void ThenTheMethodShouldReturnAListOfJsonInputs()
+    {
+        jsonInputsList = jsonReadAndWriteObject.ReadFlightsOutFromJSON(originAirport, destinationAirport, dateOfFlight);
+    }
+
+
+    [Given(@"a false OriginAirport and DestinationAirport,")]
+    public void GivenAFalseOriginAirportAndDestinationAirport()
+    {
+        originAirport = "AMSSSDD";
+        destinationAirport = "FRASA";
+    }
+
+    [When(@"the method ReadFlightsOutFromJSONoriginAirport, destinationAirport, dateOfFlight is called,")]
+    public void WhenTheMethodReadFlightsOutFromJsoNoriginAirportDestinationAirportDateOfFlightIsCalled()
+    {
+        invalidMethodArguments = () => jsonReadAndWriteObject.ReadFlightsOutFromJSON(originAirport, destinationAirport, dateOfFlight);
+    }
+
+    [Then(@"the method should throw an exception")]
+    public void ThenTheMethodShouldThrowAnException()
+    {
+        invalidMethodArguments.Should().Throw<Exception>();
+    }
+    
+}
